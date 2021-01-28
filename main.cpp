8dc554d660ac42f8faa8b770a04ddfc770edca1b
Valthonis21 <44548001+Valthonis21@users.noreply.github.com>
2021-01-27 18:46:22 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,3 +21,11 @@ int main()
 
 hello1Function
 /** hello1Function.cpp**/
+
+#include <iostream>
+using namespace std;
+int hello1Function(stringname1)
+{
+    cout << "hello World "<< name1 << endl;
+    return0;
+}
